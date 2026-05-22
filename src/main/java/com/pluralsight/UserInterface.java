8363f46dcc91e2969ae84f018dd77b435d041b70
Nurbu <lamanurbu2@gmail.com>
2026-05-22 14:57:59 -0400

    Created the processNewOrder method to display the ordering screen options and the side methods that the options would go into such as Adding pizza,drink,garlic knot,displaying the order, checking out, and cancelling the order.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -20,8 +20,8 @@ public class UserInterface {
             System.out.println(Colors.HEADER + "Welcome to the World's Best Pizzeria!!!" + Colors.RESET + "\n");
             System.out.println(BOTTOM_DASHES);
 
-            System.out.println("1. Filter by price");
-            System.out.println("12. Exit");
+            System.out.println("1. Create a new order");
+            System.out.println("2. Exit");
             System.out.print("Your choice: ");
 
             if (!scan.hasNextInt()) {
@@ -41,6 +41,55 @@ public class UserInterface {
     }
 
     private void processNewOrder() {
+        int choice = -1;
+        while (choice != 6) {
+            System.out.println(Colors.HEADER + "Order Screen!" + Colors.RESET + "\n");
+            System.out.println(BOTTOM_DASHES);
+
+            System.out.println("1. Add Pizza");
+            System.out.println("2. Add Drink");
+            System.out.println("3. Add Garlic Knot");
+            System.out.println("4. Display order");
+            System.out.println("5. Checkout");
+            System.out.println("6. Cancel order");
+            System.out.print("Your choice: ");
+
+            if (!scan.hasNextInt()) {
+                System.out.println(Colors.ERROR + "Please enter 1-6." + Colors.RESET + "\n");
+                scan.nextLine();                 // discard bad input
+                continue;
+            }
+            choice = scan.nextInt();
+            scan.nextLine();                     // clear newline
+
+            switch (choice) {
+                case 1 -> processAddPizza();
+                case 2 -> processAddDrink();
+                case 3 -> processAddGarlicKnot();
+                case 4 -> processDisplayOrder();
+                case 5 -> processCheckout();
+                case 6 -> processCancelOrder();
+                default -> System.out.println(Colors.ERROR + "Invalid choice!" + Colors.RESET + "\n");
+            }
+        }
+    }
+
+    private void processAddPizza() {
+    }
+
+    private void processAddDrink() {
+    }
+
+    private void processAddGarlicKnot() {
+    }
+
+    private void processDisplayOrder() {
+    }
+
+    private void processCheckout() {
+    }
+
+    private void processCancelOrder() {
     }
 
 
