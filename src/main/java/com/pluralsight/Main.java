e4752fe96b7b3b2fb0e7fad898493695ff3c552d
Nurbu <lamanurbu2@gmail.com>
2026-05-22 15:50:20 -0400

    Added details to Order, handled items arraylist and orderId within constructor by randomizing.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,15 @@
 package com.pluralsight;
 
+import com.pluralsight.models.Order;
+
 public class Main {
 
     public static void main(String[] args) {
+        Order order = new Order();
+        System.out.println("Order ID: " + order.getOrderId());
         UserInterface userInterface = new UserInterface();
         userInterface.Display();
+
     }
 
 }
--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -1,4 +1,37 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Item> items;
+    private int orderId;
+
+    public Order() {
+        this.items = new ArrayList<>();
+        this.orderId = setOrderId();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int setOrderId() {
+        int min = 100000, max = 200000;
+        return (int) (Math.random() * (max - min + 1)) + min;
+    }
+
+
 }
