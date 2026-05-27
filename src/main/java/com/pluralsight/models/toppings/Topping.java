241e63024a9f3b517d666e995177eddd54f0cace
Nurbu <lamanurbu2@gmail.com>
2026-05-27 11:24:27 -0400

    Created the abstract Topping class. Made getPrice abstract required to be used and override by every child and toString should only have to be overridden by the premiumToppings.

--- src/main/java/com/pluralsight/models/toppings/Topping.java
+++ src/main/java/com/pluralsight/models/toppings/Topping.java
@@ -1,4 +1,20 @@
 package com.pluralsight.models.toppings;
 
 public abstract class Topping {
+    private String name;
+
+    public Topping(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double getPrice();
+
+    @Override
+    public String toString() {
+        return this.getName() + " $" + 0.00;
+    }
 }
